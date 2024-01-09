97d8554c1763ff69acd835859fab8ca20373c01f
hp <hp@DESKTOP-0OUURVQ>
2024-01-08 23:36:42 -0800

    newcommit

---
+++ DemoJenkins/src/test/java/TestCase/DemoTestcase2.java
@@ -0,0 +1,13 @@
+package TestCase;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTestcase2 {
+
+	@Test
+	public void m2()
+	{
+		Reporter.log("m2 running",true);
+	}
+}
---
+++ DemoJenkins/src/test/java/TestCase/TC1Test.java
@@ -0,0 +1,16 @@
+package TestCase;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC1Test {
+
+	@Test
+	
+	public void m1() {
+		
+		Reporter.log("m1 excecuted",true);
+		Reporter.log("m1 excecuted",true);
+		Reporter.log("m1 excecuted",true);
+	}
+}
